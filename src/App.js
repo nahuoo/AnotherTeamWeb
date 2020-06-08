9fd378b6d4c5013c3b0c28e5b08d2f43ff329736
SylphidZoul <zoulxhikaru@gmail.com>
2020-06-07 22:33:36 -0300

    updating 3d header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header3D from './components/Header3D'
 import Navbar from './components/Navbar'
-import Jesus from './img/jebusDance.gif'
 
 const App = () => {
     return (
         <div style={{background: 'black', width: "100vw"}}>
             <Navbar />
             <Header3D />
-            <img src={Jesus} alt="Jesus" style={{background: 'transparent', position: 'absolute', top: -999999, left: -999999, bottom: -999999, right: -999999, margin: 'auto'}} />
             <div style={{color: "white"}}>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse provident id nihil perspiciatis unde nam molestiae optio! Temporibus harum molestiae consequatur dignissimos soluta labore consectetur ipsum aperiam veniam, sapiente dolorem?</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse provident id nihil perspiciatis unde nam molestiae optio! Temporibus harum molestiae consequatur dignissimos soluta labore consectetur ipsum aperiam veniam, sapiente dolorem?</p>
